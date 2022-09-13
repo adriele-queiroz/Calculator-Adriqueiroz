eb4f7fb25de9cfb27cb52ace4d94cfc94ba6cd4d
Rogério M. Queiroz <rogerio.mq@gmail.com>
2022-09-13 16:24:20 -0300

    ref: update function using camelCase pattern; feat: add cleanDisplay function;

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function displaynum(n1){
+function displayNum(n1){
     Calculator.text1.value = Calculator.text1.value + n1;
+}
+
+function cleanDisplay() {
+    const inputText1 = document.getElementsByName('text1')[0];
+    console.log(inputText1);
+    if(inputText1) {
+        inputText1.value = '';
+    }
 }

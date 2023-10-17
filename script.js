644ed117181c5b584db57cc5e9d5cd2cfe6c0322
Adriele Queiroz <105941726+adriele-queiroz@users.noreply.github.com>
2023-10-17 13:18:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-function displayNum(n1){
-    Calculator.text1.value = Calculator.text1.value + n1;
+const display = document.getElementById('display');
+const historyTable = document.getElementById('history-table');
+let expression = '';
+
+function displaynum(value){
+    expression += value;
+    display.value = expression;
 }
 
-function cleanDisplay() {
-    const inputText1 = document.getElementsByName('text1')[0];
-    console.log(inputText1);
-    if(inputText1) {
-        inputText1.value = '';
+function calculate(){
+    try {
+        const result = eval(expression);
+        display.value = result;
+        addToHistory(expression, result);
+        expression = '';
+    } catch (error) {
+        display.value = 'Erro';
+        expression = '';
     }
 }
+
+function addToHistory(expression, result) {
+    const dateTime = new Date().toLocaleString(); // Obtém a data e hora atual
+    const row = historyTable.insertRow(1);
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    const cell3 = row.insertCell(2);
+    const cell4 = row.insertCell(3);
+    cell1.innerHTML = dateTime;
+    cell2.innerHTML = expression;    
+}
+
+function replaceDisplay(newExpression, newResult) {
+    display.value = newExpression;
+    expression = newExpression;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    display.value = '';
+});

7715c5ee30ae58f0a9179bfa2c0be3d3d201232c
Adriele Queiroz <105941726+adriele-queiroz@users.noreply.github.com>
2023-10-30 13:21:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -19,14 +19,24 @@ function calculate(){
     }
 }
 
-function addToHistory(expression) {
+function addToHistory(expression, result) {
     const dateTime = new Date().toLocaleString(); // Obtém a data e hora atual
     const row = historyTable.insertRow(1);
     const cell1 = row.insertCell(0);
     const cell2 = row.insertCell(1);
+    const cell3 = row.insertCell(2);
+    const cell4 = row.insertCell(3);
     cell1.innerHTML = dateTime;
-    cell2.innerHTML = expression;    
+    cell2.innerHTML = expression;
+    cell3.innerHTML = result;
+    cell4.innerHTML = '<button onclick="replaceDisplay(\'' + expression + '\', ' + result + ')">Reproduzir</button>';
 }
 
+function replaceDisplay(newExpression, newResult) {
+    display.value = newExpression;
+    expression = newExpression;
+}
 
-
+document.addEventListener('DOMContentLoaded', function () {
+    display.value = '';
+});
